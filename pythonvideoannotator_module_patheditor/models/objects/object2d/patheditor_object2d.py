0cbf9203dd2478b76eddef465fb6160e4c2b0808
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-11-28 11:52:10 +0000

    .

--- pythonvideoannotator_module_patheditor/models/objects/object2d/patheditor_object2d.py
+++ pythonvideoannotator_module_patheditor/models/objects/object2d/patheditor_object2d.py
@@ -25,10 +25,10 @@ class PathEditorObject2d(object):
 	######################################################################
 
 	def create_tree_nodes(self):
-		self.treenode = self.tree.createChild(self.name, icon=conf.ANNOTATOR_ICON_OBJECT )
-		self.tree.addPopupMenuOption(
+		self.treenode = self.tree.create_child(self.name, icon=conf.ANNOTATOR_ICON_OBJECT )
+		self.tree.add_popup_menu_option(
 			label='Create a path dataset', 
-			functionAction=self.create_path_dataset, 
+			function_action=self.create_path_dataset, 
 			item=self.treenode, icon=conf.ANNOTATOR_ICON_TIMELINE
 		)
 		self.treenode.win = self
@@ -37,10 +37,10 @@ class PathEditorObject2d(object):
 
 	def create_motion_tree_nodes(self):
 		
-		self.treenode_motion = self.tree.createChild('Motion', icon=conf.ANNOTATOR_ICON_PATH, parent=self.treenode )
-		variation_treenode 	 = self.tree.createChild('x', icon=conf.ANNOTATOR_ICON_X, parent=self.treenode_motion )
-		self.tree.addPopupMenuOption(label='View on the timeline', functionAction=self.__send_motion_to_timeline_evt, item=variation_treenode, icon=conf.ANNOTATOR_ICON_TIMELINE)
-		self.tree.addPopupMenuOption(label='View on the timeline', functionAction=self.__send_motion_variation_to_timeline_evt, item=variation_treenode, icon=conf.ANNOTATOR_ICON_TIMELINE)
+		self.treenode_motion = self.tree.create_child('Motion', icon=conf.ANNOTATOR_ICON_PATH, parent=self.treenode )
+		variation_treenode 	 = self.tree.create_child('x', icon=conf.ANNOTATOR_ICON_X, parent=self.treenode_motion )
+		self.tree.add_popup_menu_option(label='View on the timeline', function_action=self.__send_motion_to_timeline_evt, item=variation_treenode, icon=conf.ANNOTATOR_ICON_TIMELINE)
+		self.tree.add_popup_menu_option(label='View on the timeline', function_action=self.__send_motion_variation_to_timeline_evt, item=variation_treenode, icon=conf.ANNOTATOR_ICON_TIMELINE)
 		
 		self.treenode_motion.obj = variation_treenode.obj = self
 
--- pythonvideoannotator_module_patheditor/module.py
+++ pythonvideoannotator_module_patheditor/module.py
@@ -2,7 +2,7 @@ import cv2, os
 from pysettings import conf
 from PyQt4 import QtCore, QtGui
 from pythonvideoannotator.utils.tools import list_folders_in_path
-from pythonvideoannotator.models.objects import Objects
+from pythonvideoannotator_models_gui.models import Project
 
 from pyforms.Controls import ControlDockWidget
 
@@ -14,17 +14,17 @@ class Module(object):
 		"""
 		super(Module, self).__init__()
 
-		self._right_docker          = ControlDockWidget('Objects list',side=ControlDockWidget.SIDE_RIGHT, order=0, margin=5)        
+		self._right_docker          = ControlDockWidget('Videos list',side=ControlDockWidget.SIDE_RIGHT, order=0, margin=5)        
 		self._right_details         = ControlDockWidget('Details',side=ControlDockWidget.SIDE_RIGHT, order=1, margin=5)        
 		
-		self._right_docker.value    = self._objects_window = Objects(parent=self)
+		self._right_docker.value    = self._project = Project(parent=self)
 		#self._right_docker.hide()
 		#self._right_details.hide()
 
 	
 		self.mainmenu.insert(2,
 			{'Windows': [
-				{'Objects': self.__show_objects_list_evt, 'icon':conf.ANNOTATOR_ICON_OBJECT }
+				{'Videos': self.__show_objects_list_evt, 'icon':conf.ANNOTATOR_ICON_OBJECT }
 			]}
 		)
 
@@ -33,14 +33,15 @@ class Module(object):
 		self._right_details.show()
 
 	def onPlayerClick(self, event, x, y): 
-		self._objects_window.player_on_click(event, x, y)
+		self._project.player_on_click(event, x, y)
 
 
 	def process_frame(self, frame):
 		"""
 		Function called before render each frame
 		"""
-		self._objects_window.draw(frame, self._player.video_index)
+		if self._player.video_index is not None: 
+			self._project.draw(frame, self._player.video_index)
 		return frame
 
 	def add_object_evt(self, obj): 	 	pass
@@ -50,7 +51,7 @@ class Module(object):
 	def remove_dataset_evt(self, dataset): pass
 	
 
-	def add_chart(self, name, data):  	 self._time.add_chart(name, data)
+	def add_graph(self, name, data):  	 self._time.add_graph(name, data)
 
 	
 	######################################################################################
@@ -60,18 +61,14 @@ class Module(object):
 	
 	def save(self, data, project_path=None):
 		data = super(Module, self).save(data, project_path)
-		
-		objects_path = os.path.join(project_path, 'objects')
-		if not os.path.exists(objects_path): os.makedirs(objects_path)
-		
-		return self._objects_window.save(data, objects_path)
+		self._project.save(data, project_path)
+		return data
 
 
 	def load(self, data, project_path=None):
 		data = super(Module, self).load(data, project_path)
-
-		objects_path = os.path.join(project_path, 'objects')
-		self._objects_window.load(data, objects_path)
+		self._project.load(data, project_path)
+		
 
 
 	######################################################################################
@@ -79,7 +76,7 @@ class Module(object):
 	######################################################################################
 	
 	@property
-	def objects(self): return self._objects_window.objects
+	def objects(self): return self._project.objects
 
 	@property
 	def details(self): return self._right_details.value
